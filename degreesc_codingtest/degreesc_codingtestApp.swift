6f026a284bce2c0c9e09081e08d41a3ac7612223
beobmun <gksqjqans1@naver.com>
2022-04-04 15:37:35 +0900

    Initial Commit

---
+++ degreesc_codingtest/degreesc_codingtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  degreesc_codingtestApp.swift
+//  degreesc_codingtest
+//
+//  Created by 한법문 on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct degreesc_codingtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
