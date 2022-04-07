6ba44f7b762fe7c312d95db654791cd829f2114c
beobmun <gksqjqans1@naver.com>
2022-04-07 14:15:47 +0900

    Initial Commit

---
+++ Local_Weather/Local_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_WeatherApp.swift
+//  Local_Weather
+//
+//  Created by 한법문 on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct Local_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
